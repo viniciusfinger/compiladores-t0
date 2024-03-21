95d3522ff5f2aabcd111efd83aa4d033447db7a1
Vinicius Finger <fingervinicius@icloud.com>
2024-03-20 22:26:16 -0300

    procura menor feature

--- src/zero.py
+++ src/zero.py
@@ -1,12 +1,20 @@
 # SPDX-License-Identifer: MIT
 
-"""Implementação do exercício zero."""
-
-
 def procura_maior(lista):
-    """Procura maior item na lista usando procura linear."""
     maior = lista[0]
+    
     for item in lista[1:]:
         if item > maior:
             maior = item
+    
     return maior
+
+def procura_menor(lista):
+    menor = lista[0]
+    
+    for item in lista[1:]:
+        if item < menor:
+            menor = item
+    
+    return menor
+
