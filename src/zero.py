44499e619d0a8d4fa3c51b9ac855073ec0a980b1
Vinicius Finger <fingervinicius@icloud.com>
2024-03-28 10:50:48 -0300

    Removing blank spaces in empty lines

--- src/zero.py
+++ src/zero.py
@@ -3,21 +3,21 @@
 def procura_maior(lista):
     """Retorna o maior elemento de uma lista."""
     maior = lista[0]
-    
+
     for item in lista[1:]:
         if item > maior:
             maior = item
-    
+
     return maior
 
 def procura_menor(lista):
     """Retorna o menor elemento de uma lista."""
     menor = lista[0]
-    
+
     for item in lista[1:]:
         if item < menor:
             menor = item
-    
+
     return menor
 
 def procura_impares(lista):
@@ -27,7 +27,7 @@ def procura_impares(lista):
     for item in lista:
         if item % 2 != 0:
             elementos_impares.append(item)
-    
+
     return elementos_impares
 
 def procura_pares(lista):
@@ -37,5 +37,5 @@ def procura_pares(lista):
     for item in lista:
         if item % 2 == 0:
             elementos_pares.append(item)
-    
+
     return elementos_pares
